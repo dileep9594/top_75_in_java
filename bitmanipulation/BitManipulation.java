8180dbb095ba279121a6cb77f2181e02002d4c83
dileep <dileep9594@gmail.com>
2024-09-29 17:25:06 +0530

    backing up

---
+++ bitmanipulation/BitManipulation.java
@@ -0,0 +1,25 @@
+package bitmanipulation;
+
+public class BitManipulation {
+    public int singleNumber(int[] nums) {
+        
+    }
+    public int hammingWeight(int n) {
+        
+    }
+    public int[] countBits(int n) {
+        
+    }
+    public int reverseBits(int n) {
+        
+    }
+    public int missingNumber(int[] nums) {
+        
+    }
+    public int getSum(int a, int b) {
+        
+    }
+    public int reverse(int x) {
+        
+    }
+}
